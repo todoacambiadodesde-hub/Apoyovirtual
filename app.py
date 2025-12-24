cba507938a367d2bfb3c831501cbbe1a43f32a38
Jesus Solis Cid <mr.transe@I-dont-wanna-friends.local>
2025-12-24 01:20:16 -0600

    Corrigiendo error de sangria en el puerto

--- app.py
+++ app.py
@@ -248,4 +248,4 @@ if __name__ == '__main__':
                     f.write('LISTA_PREGUNTAS = [{"e": "Pregunta de prueba", "r": "0"}]')
 
    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+   app.run(host='0.0.0.0', port=port)
